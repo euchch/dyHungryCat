6b4d8593cc039fe57ff593378f9cc7fc857f48c5
euchch <yaakov.yehezkel@gmail.com>
2017-07-06 20:08:07 +0300

    last update before letting it run as lambda function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
+from __future__ import print_function
+
 import os
+import json
+import urllib
+import boto3
 from utils.hcGoogleCloudApi import get_vision_api_stub #, get_vision_api
 from utils.hcAwsS3Api import list_bucket # , getStatsJson, setStatsJson
 from utils.hcImagesApi import read_image_base64
@@ -25,11 +29,20 @@ def updateDb(config, key):
 	stats['notificationSentTimeStamp'] = "1970-01-01T00:00:00.000Z"
 	addRecord(config, stats)
 
-def testImage(config, key):
+def testImage(config, bucket, key):
+	# response has the image content to upload to google-vision
+	try:
+		# response = s3.get_object(Bucket=bucket, Key=key)
+		response = 0
+	except Exception as e:
+		print(e)
+		print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+		raise e
+
 	gVisionResponse = json.loads(get_vision_api_stub(key))
 	gVisionLabels = gVisionResponse['responses'][0]['labelAnnotations']
 	for label in gVisionLabels:
-		print label['description'], label['score'] , 'Searching for: ' , config['feeding']['label'], config['feeding']['minimumScore']
+		print (label["description"], label["score"] , 'Searching for: ' , config['feeding']['label'], config['feeding']['minimumScore'])
 		if (label['description'] != config['feeding']['label']):
 			continue
 		if (label['score'] * 100 < config['feeding']['minimumScore']):
@@ -39,7 +52,7 @@ def testImage(config, key):
 
 	return False
 
-def main():
+def mainTest():
 	from pprint import pprint
 	config=readConfigs()
 	bucketFilesList = list_bucket(config)
@@ -48,7 +61,20 @@ def main():
 		if not fileName.endswith(config['feeding']['imageSuffix']):
 			continue
 		pprint(vars(key))
-		if testImage(config, key):
+		if testImage(config, config['s3']['bucket'], key):
 			updateDb(config, key)
 
-main()
+def lambda_handler(event, context):
+	from pprint import pprint
+	pprint(vars(event))
+	pprint(vars(context))
+	config=readConfigs()
+	bucket = event["Records"][0]["s3"]["bucket"]["name"]
+	key = urllib.unquote_plus(event["Records"][0]["s3"]["object"]["key"].encode("utf8"))
+	pprint(vars(bucket))
+	pprint(vars(key))
+	if testImage(config, bucket, key):
+		updateDb(config, key)
+
+print('Loading function')
+s3 = boto3.client('s3')
--- utils/hcMysqlApi.py
+++ utils/hcMysqlApi.py
@@ -2,17 +2,28 @@ import pymysql.cursors
 import pymysql
 
 def init(config):
-    global connection
     connection = pymysql.connect(host=config['db']['host'],
                              user='root',
                              password=config['passwords']['rdsPassword'],
                              db=config['db']['db'],
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+# please note that you're supposed to get a connection here - so you're suppose to close it after using the function!
+def isLatestDate(config, feedingDate, cursor):
+    sql = "SELECT COUNT(*) as count FROM " + config['db']['table'] + " WHERE 'lastModified' >= '" + feedingDate + "'"
+    print sql
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if (result["count"] > 0):
+        print ("Cat was fed at a later time than " + feedingDate + ", skipping...")
+        return False
+    return True
 
 def printRecords(config):
+    connection = init(config)
     try:
-        init (config)
         with connection.cursor() as cursor:
         # Read a single record
             sql = "SELECT * FROM " + config['db']['table']
@@ -23,10 +34,12 @@ def printRecords(config):
         connection.close()
 
 def addRecord(config, stats):
+    connection = init(config)
     try:
-        init (config)
         with connection.cursor() as cursor:
         # Read a single record
+            if not isLatestDate(config, stats['lastModified'], cursor):
+                return
             sql = "INSERT INTO " + config['db']['table'] + "(`foodName`, `updateTime`, `notificationSent`, `lastModified`, `notificationSentTimeStamp`) VALUES (%s, %s, %s, %s, %s)"
             cursor.execute(sql, (stats['foodName'], stats['updateTime'], stats['notificationSent'], stats['lastModified'], stats['notificationSentTimeStamp']))
             connection.commit()
